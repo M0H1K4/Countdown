ad4a6c8ffd4df413fcba389d68abae10aa7b8279
M0H1K4N <nejer911@gmail.com>
2023-10-31 22:33:27 +0400

    add Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,26 +4,18 @@ export default function Player() {
   const playerName = useRef();
 
   const [enteredPlayerName, setEnteredplayerName] = useState(null);
-  const [submited, setSubmited] = useState(false);
-
-  function handleChange(event) {
-    setSubmited(false);
-    setEnteredplayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmited(true);
+    setEnteredplayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submited ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input
           ref={playerName}
           type="text"
-          onChange={handleChange}
-          value={enteredPlayerName}
         />
         <button onClick={handleClick}>Set Name</button>
       </p>
