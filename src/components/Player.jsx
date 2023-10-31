407d0a8669ce74c880f1d9fa9d5090df634dcb05
M0H1K4N <nejer911@gmail.com>
2023-10-31 22:12:39 +0400

    states is done in input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 
 export default function Player() {
   const [enteredPlayerName, setEnteredplayerName] = useState("");
+  const [submited, setSubmited] = useState(false);
+
 
   function handleChange(event) {
     setEnteredplayerName(event.target.value);
   }
 
+  function handleClick(){
+    setSubmited(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submited ? enteredPlayerName:  'unknown entity'}</h2>
       <p>
         <input type="text" onChange={handleChange} value={enteredPlayerName} />
-        <button>Set Name</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
