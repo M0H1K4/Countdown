120bfb59956b01f9baa0ba9a3a029f6ced182142
M0H1K4N <nejer911@gmail.com>
2023-10-31 22:08:39 +0400

    states update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 export default function Player() {
- const [enteredPlayerName, setEnteredplayerName] = useState('');
+  const [enteredPlayerName, setEnteredplayerName] = useState("");
+
+  function handleChange(event) {
+    setEnteredplayerName(event.target.value);
+  }
+
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
-        <input type="text" />
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
         <button>Set Name</button>
       </p>
     </section>
