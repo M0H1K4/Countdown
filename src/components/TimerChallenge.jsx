6c330b6e1f43476c5f04ece18f8378b4ed35057a
M0H1K4N <nejer911@gmail.com>
2023-11-01 21:31:25 +0400

    Another way of using states

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,16 +6,26 @@ import ResultModal from "./ResultModal";
 export default function TimerChallange({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
+
   const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-  function handleStart() {
-    timer.current = setInterval(() => {}, 10);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-    setTimerStarted(true);
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemainig) => prevTimeRemainig - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
@@ -27,12 +37,12 @@ export default function TimerChallange({ title, targetTime }) {
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running... " : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running... " : "Timer inactive"}
         </p>
       </section>
     </>
