5816326477142d6d4ae835886705ad94889f0d03
M0H1K4N <nejer911@gmail.com>
2023-11-01 16:42:36 +0400

    add: configuration of challenge file

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function TimerChallange({title}) {
+export default function TimerChallange({title, targetTime}) {
   return (
     <section className='challange'>
       <h2>{title}</h2>
       <p className="challange-time">
-        
+        {targetTime} seconds
       </p>
     </section>
   )
