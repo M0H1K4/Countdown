d5d8f74806fb86d592c9d2c831958686ed4cc86d
M0H1K4N <nejer911@gmail.com>
2023-11-01 16:51:55 +0400

    @

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,12 +5,12 @@ export default function TimerChallange({ title, targetTime }) {
     <section className="challenge">
       <h2>{title}</h2>
       <p className="challenge-time">
-        {targetTime} seconds{targetTime > 1 ? "s" : ""}
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
         <button>Start Challenge</button>
       </p>
-      <p className="active">Time is running... / Timer inactive</p>
+      <p className="active">Time is running... /Timer inactive</p>
     </section>
   );
 }
