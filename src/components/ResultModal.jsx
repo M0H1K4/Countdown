b0357185ccd3d08f4d66ecaff6250b13edad5ea6
M0H1K4N <nejer911@gmail.com>
2023-11-01 21:38:37 +0400

    bug

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,13 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime, remainingTime },
+  {  targetTime, remainingTime },
   ref
 ) {
   const dialog = useRef();
 
     const userLost = remainingTime <= 0;
-
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -24,7 +24,7 @@ const ResultModal = forwardRef(function ResultModal(
         The target time was <strong>{targetTime} seconds.</strong>{" "}
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>{" "}
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>{" "}
       </p>
       <form method="dialog">
         <button>Close</button>
